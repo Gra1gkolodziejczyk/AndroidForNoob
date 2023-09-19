aaf12c1a6b97d909c10af9709fd0cc78a01d3c87
Gra1gkolodziejczyk <graig.kolodziejczyk@ynov.com>
2023-09-19 13:42:57 +0200

    add a new image and a new design homepage

--- AndroidForNoob/app/src/main/java/com/example/androidfornoob/MainActivity.kt
+++ AndroidForNoob/app/src/main/java/com/example/androidfornoob/MainActivity.kt
@@ -3,41 +3,61 @@ package com.example.androidfornoob
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.androidfornoob.ui.theme.AndroidForNoobTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val image: Painter = painterResource(id = R.drawable.android)
             AndroidForNoobTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.White
                 ) {
-                    Greeting("Android")
+                    Box(
+                        modifier = Modifier
+                            .background(Color.White)
+                            .fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .background(Color.White)
+                                .fillMaxWidth(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                        ) {
+                            Image(
+                                painter = image,
+                                contentDescription = "Description de l'image Android",
+                                modifier = Modifier
+                                    .size(200.dp),
+                                contentScale = ContentScale.Crop
+                            )
+                            Text(
+                                "J'adore le développement Android !",
+                                fontSize = 20.sp,
+                                color = Color.Green
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidForNoobTheme {
-        Greeting("Android")
     }
 }
